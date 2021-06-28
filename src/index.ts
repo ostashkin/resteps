9140c56b598d68a3704bdd07432523e3c8c36766
Pavel Ostashkin <ostashpav@gmail.com>
2021-06-28 15:59:45 +0300

    refactor: optimized imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
+import { StepsAPI } from './types/api';
+import { StepsConfig } from './types/config';
+import { ConnectedStepProps, StepComponentProps } from './types/ConnectedStep';
+import { StepsContext } from './types/context';
+import { DebugProps } from './types/debug';
+import { StepsInfo, StepsBooleanInfo } from './types/info';
+import { StepsComputedProps, StepsChildrenProps } from './types/props';
+import { StepsState } from './types/state';
+import { StepsBase } from './types/steps';
+import { StepAPI, UseStepResult } from './types/useStep';
+
 import { Steps } from './Steps';
 import { connect } from './connect';
 
-export { Steps, connect };
+export {
+  StepsAPI,
+  StepsConfig,
+  ConnectedStepProps,
+  StepComponentProps,
+  StepsContext,
+  DebugProps,
+  StepsInfo,
+  StepsBooleanInfo,
+  StepsComputedProps,
+  StepsChildrenProps,
+  StepsState,
+  StepsBase,
+  StepAPI,
+  UseStepResult,
+  Steps,
+  connect,
+};
